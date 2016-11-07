43802066a67f69495e2ed3164dde4d94c6dd438e
User <user@ws0441.local>
2016-11-07 14:33:39 +0700

    NSManagedObject+ASDataSync selectObjectsFromContext methods

--- CommonClasses/NSManagedObject+ASDataSync.h
+++ CommonClasses/NSManagedObject+ASDataSync.h
@@ -9,8 +9,20 @@
 #import <CoreData/CoreData.h>
 #import "ASynchronizable.h"
 
+typedef void (^FetchArray)(NSArray <__kindof NSManagedObject <ASynchronizableObject> *> *objects);
+
 @interface NSManagedObject (ASDataSync)
 
 - (NSString *)UUIDString;
+
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context fetch:(FetchArray)fetch;
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context limit:(NSUInteger)limit fetch:(FetchArray)fetch;
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context orderBy:(NSArray <NSSortDescriptor *> *)sortDescriptors fetch:(FetchArray)fetch;
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context orderBy:(NSArray <NSSortDescriptor *> *)sortDescriptors limit:(NSUInteger)limit fetch:(FetchArray)fetch;
+
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context where:(NSPredicate *)clause fetch:(FetchArray)fetch;
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context where:(NSPredicate *)clause limit:(NSUInteger)limit fetch:(FetchArray)fetch;
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context where:(NSPredicate *)clause orderBy:(NSArray <NSSortDescriptor *> *)sortDescriptors fetch:(FetchArray)fetch;
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context where:(NSPredicate *)clause orderBy:(NSArray <NSSortDescriptor *> *)sortDescriptors limit:(NSUInteger)limit fetch:(FetchArray)fetch;
 
 @end
--- CommonClasses/NSManagedObject+ASDataSync.m
+++ CommonClasses/NSManagedObject+ASDataSync.m
@@ -7,6 +7,7 @@
 //
 
 #import "NSManagedObject+ASDataSync.h"
+#import "NSString+LogLevel.h"
 
 @implementation NSManagedObject (ASDataSync)
 
@@ -18,6 +19,47 @@ NSString *uuidString;
         uuidString = ((NSUUID *)[NSKeyedUnarchiver unarchiveObjectWithData:data]).UUIDString;
     }
     return uuidString;
+}
+
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context fetch:(FetchArray)fetch {
+    [self selectObjectsFromContext:context limit:0 fetch:fetch];
+}
+
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context limit:(NSUInteger)limit fetch:(FetchArray)fetch {
+    [self selectObjectsFromContext:context orderBy:nil limit:limit fetch:fetch];
+}
+
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context orderBy:(NSArray<NSSortDescriptor *> *)sortDescriptors fetch:(FetchArray)fetch {
+    [self selectObjectsFromContext:context orderBy:sortDescriptors limit:0 fetch:fetch];
+}
+
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context orderBy:(nullable NSArray <NSSortDescriptor *> *)sortDescriptors limit:(NSUInteger)limit fetch:(FetchArray)fetch {
+    [self selectObjectsFromContext:context where:nil orderBy:sortDescriptors limit:limit fetch:fetch];
+}
+
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context where:(NSPredicate *)clause fetch:(FetchArray)fetch {
+    [self selectObjectsFromContext:context where:clause limit:0 fetch:fetch];
+}
+
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context where:(NSPredicate *)clause limit:(NSUInteger)limit fetch:(FetchArray)fetch {
+    [self selectObjectsFromContext:context where:clause orderBy:nil limit:limit fetch:fetch];
+}
+
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context where:(NSPredicate *)clause orderBy:(nullable NSArray <NSSortDescriptor *> *)sortDescriptors fetch:(FetchArray)fetch {
+    [self selectObjectsFromContext:context where:clause orderBy:sortDescriptors limit:0 fetch:fetch];
+}
+
++ (void)selectObjectsFromContext:(NSManagedObjectContext *)context where:(NSPredicate *)clause orderBy:(nullable NSArray <NSSortDescriptor *> *)sortDescriptors limit:(NSUInteger)limit fetch:(FetchArray)fetch {
+    [context performBlock:^{
+        NSFetchRequest *request = [self fetchRequest];
+        request.predicate = clause;
+        [request setSortDescriptors:sortDescriptors];
+        [request setFetchLimit:limit];
+        NSError *error = nil;
+        NSArray *entities = [context executeFetchRequest:request error:&error];
+        if (error) [[NSString stringWithFormat:@"%s %@\n%@", __PRETTY_FUNCTION__, error.localizedDescription, error.userInfo] logError];
+        fetch(entities);
+    }];
 }
 
 @end
